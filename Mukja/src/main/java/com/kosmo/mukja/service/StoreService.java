abb392334a012df9fed9fb1b14b30d2e2e09b5ae
shim <shim8934@naver.com>
2020-07-14 19:44:33 +0900

    0714 sm

--- Mukja/src/main/java/com/kosmo/mukja/service/StoreService.java
+++ Mukja/src/main/java/com/kosmo/mukja/service/StoreService.java
@@ -7,6 +7,9 @@ public interface StoreService {
 	
 	public List<StoreDTO> getStoreInfo(Map map);
 	public int getReviewCount(Map map);
+	
+	
+	
 	public List<FoodMenuDTO> getFoodMenu(Map map);
 	public List<StoreIMGDTO> getStoreIMG(Map map);
 	public List<FoodIMGDTO> getFoodIMG(String menu_no);
